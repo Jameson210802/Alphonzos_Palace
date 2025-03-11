267c6fb43d504867ed596b7a13854b57bf7bacfa
Jameson210802 <jamie.anderson210802@gmail.com>
2025-03-10 19:05:56 -0500

    working on animations and walls.
    trying to get animations and walls to work with goblin. Goblin does idle animation when moving.

--- Assets/Enemy_Character_1/Enemy_Back_and_Forth.cs
+++ Assets/Enemy_Character_1/Enemy_Back_and_Forth.cs
@@ -4,6 +4,7 @@ public class Enemy_Back_and_Forth : MonoBehaviour
 {
     public float movement_speed = 3.0f;
     public bool going_right = true;
+    public float mRaycastingDistance = 1f;
 
     private SpriteRenderer _mSpriteRenderer;
 
@@ -20,8 +21,25 @@ public class Enemy_Back_and_Forth : MonoBehaviour
         directionTranslation *= Time.deltaTime * movement_speed;
 
         transform.Translate(directionTranslation);
+        CheckForWalls();
+    }
+    private void CheckForWalls()
+    {
+        Vector3 raycastDirection = (going_right) ? Vector3.right : Vector3.left;
 
+        // Raycasting takes as parameters a Vector3 which is the point of origin, another Vector3 which gives the direction, and finally a float for the maximum distance of the raycast
+        RaycastHit2D hit = Physics2D.Raycast(transform.position + raycastDirection * mRaycastingDistance - new Vector3(0f, 0.25f, 0f), raycastDirection, 0.075f);
 
-        //CheckForWalls();
+        // if we hit something, check its tag and act accordingly
+        if (hit.collider != null)
+        {
+            if (hit.transform.tag == "Terrain")
+            {
+                going_right = !going_right;
+                _mSpriteRenderer.flipX = going_right;
+
+            }
+        }
     }
 }
+
