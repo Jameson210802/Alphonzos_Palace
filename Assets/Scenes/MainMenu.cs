c8c8eebf731acaaae230e72ac1c75e13e2e8f405
Rockvillager <akiradira10@gmail.com>
2025-03-13 22:37:44 -0500

    Added First Stage BGM
    - Added first stage BGM when play button is clicked (May have to change how this gets triggered depending on how levels are implemented)

--- Assets/Scenes/MainMenu.cs
+++ Assets/Scenes/MainMenu.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public AudioSource titleBGM;
+    public AudioSource checkpointZeroBGM;
+
     public void PlayGame()
     {
         titleBGM.Stop();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        checkpointZeroBGM.Play();
     }
 
     public void QuitGame()
