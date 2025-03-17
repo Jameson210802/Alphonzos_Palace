820a97634a46fd7b1d00b45ad93f75f7de80220f
Brayden-Mendez <bdm458@msstate.edu>
2025-03-17 02:34:54 -0500

    Checkpoint added

---
+++ Assets/checkPoint.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class checkPoint : MonoBehaviour
+{
+    public Rigidbody2D rb; 
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("You Win!");
+        }
+    }
+}
