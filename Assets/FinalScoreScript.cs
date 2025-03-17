2e38e3fd9f22fd22c69665f1930d0612453b6b25
Brayden-Mendez <bdm458@msstate.edu>
2025-03-17 14:39:52 -0500

    Trying to create final score on win screen

---
+++ Assets/FinalScoreScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinalScoreScript : MonoBehaviour
+{
+   /* 
+    public ScoreScript playerScore;
+    public Text scoreText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        string finalScore = playerScore.playerScore.toString();
+        scoreText.text = finalScore.ToString();
+    } 
+    */
+}
