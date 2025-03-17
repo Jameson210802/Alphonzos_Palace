6eb1b0e51345cd73eeca950cead3faf52f3d020a
Brayden-Mendez <bdm458@msstate.edu>
2025-03-17 00:39:49 -0500

    Added Coin Asset

---
+++ Assets/CoinScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
